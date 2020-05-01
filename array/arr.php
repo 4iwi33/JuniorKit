603371ccdd970008a26b32a37f230f17129b7264
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-05-01 17:22:32 +0300

    create arr

---
+++ array/arr.php
@@ -0,0 +1,21 @@
+<?php
+
+$months = ["January", "February", "March", "April", "May"];
+$person = [
+    "name" => "John ",
+    "surname" => "Doe ",
+    "age" => 30,
+    "months" => [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May"
+    ]
+];
+
+// echo $person["name"];
+// echo $person["surname"];
+// echo $person["age"];
+
+echo $person["months"][3];
