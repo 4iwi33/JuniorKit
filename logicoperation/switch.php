3c6f5738ad0896668b94f22a4274a93d067c059d
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-05-01 13:35:43 +0300

    add

---
+++ logicoperation/switch.php
@@ -0,0 +1,13 @@
+<?php
+
+switch ($super) {
+    case "Cool":
+        echo "Super равно Cool";
+        break;
+    case "Nice":
+        echo "Super равно Nice";
+        break;
+    case "Bad":
+        echo "Super равно Bad";
+        break;
+}
