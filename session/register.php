b75a18553c997d88a56d1a5f8731b5f5601cf92a
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-05-03 16:27:19 +0300

    adding

---
+++ session/register.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="register.php" method="POST">
+        <p>
+            <label>Username</label>
+            <input type="text" name="username">
+        </p>
+        <p>
+            <label>E-mail:</label>
+            <input type="text" name="email">
+        </p>
+        <p>
+            <label>Password:</label>
+            <input type="text" name="password">
+        </p>
+        <p>
+            <label>Repeat password:</label>
+            <input type="text" name="reapet_password">
+        </p>
+        <input type="submit" value="Register">
+    </form>
+</body>
+
+</html>
